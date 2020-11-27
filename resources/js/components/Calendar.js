24c34d6d5c19da16f7982e7d549eebc135d0b20f
Pavel <pvolkv@gmail.com>
2020-11-27 21:44:29 +0300

    :tada:

--- resources/js/components/Calendar.js
+++ resources/js/components/Calendar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import HorizontalCalendar from "../vendor/HorizontalCalendar";
 
-
 const Calendar = () => {
 
     return (
 
         <HorizontalCalendar
             date={new Date("2020-9-17")}
-            choosed='1'
+            choosed={2}
             onClick={({choosedDay, dayNumber}) => {
                 console.log(choosedDay + ' ' + dayNumber)
             }}
