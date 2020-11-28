750e17b03bf5294545b84442188b72f11d895cbe
rmekhtiev <mekhtievrs0@gmail.com>
2020-11-28 18:53:15 +0300

    fix: add hover

--- resources/js/components/MatchItem.js
+++ resources/js/components/MatchItem.js
@@ -16,31 +16,29 @@ const Match = ({teamHome, teamAway, startTime, endTime, scores, match, teamHomeI
     console.log(gameIcon)
 
     return (
-        <Div onClick={onClick} data-to="match">
-            <div className="match__row">
-                <div className="match__game">
-                    <TeamLogo background={gameIcon} width={'25px'} height={'25px'}/>
-                </div>
-                <div className="match__teams">
-                    <div className="match__team">
-                        <TeamLogo background={teamHome.images.default}/>
-                        <div>
-                            {teamHome.name}
-                        </div>
-                    </div>
-                    <div className="match__score">
-                        <div>{scores[teamHomeId]} : {scores[teamAwayId]}</div>
-                    </div>
-                    <div className="match__team">
-                        <TeamLogo background={teamAway.images.default}/>
-                        <div>
-                            {teamAway.name}
-                        </div>
+        <Div onClick={onClick} data-to="match" className="match__row">
+            <div className="match__game">
+                <TeamLogo background={gameIcon} width={'25px'} height={'25px'}/>
+            </div>
+            <div className="match__teams">
+                <div className="match__team">
+                    <TeamLogo background={teamHome.images.default}/>
+                    <div>
+                        {teamHome.name}
                     </div>
                 </div>
-                <div className="match__date">
-                    {startTime}
+                <div className="match__score">
+                    <div>{scores[teamHomeId]} : {scores[teamAwayId]}</div>
                 </div>
+                <div className="match__team">
+                    <TeamLogo background={teamAway.images.default}/>
+                    <div>
+                        {teamAway.name}
+                    </div>
+                </div>
+            </div>
+            <div className="match__date">
+                {startTime}
             </div>
         </Div>
     );
